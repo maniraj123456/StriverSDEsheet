3a9dce76adb56c21a97cecebdbbebe412ffe8029
maniraj123456 <lingalamaniraj123@gmail.com>
2023-10-13 16:53:18 +0530

    Update Implement Stack using Arrays.java
    Aray okkari chudu raa nen pettina comments and changes

--- StackAndQueues/Implement Stack using Arrays.java
+++ StackAndQueues/Implement Stack using Arrays.java
@@ -1,33 +1,40 @@
 public class Solution{
     static class Stack {
         int arr[];
-        int top1 = -1;
-        int cap1 = 0;
+        //pointer to stack top
+        int top1 = -1;//Intially it is set to no elemnets
+        int cap1 = 0;//Intial capacity is 0;
         Stack(int capacity) {
             arr = new int[capacity];
             cap1 = capacity;
         }
-        public void push(int num) {
+        public boolean push(int num) {
             if(top1==cap1-1){
-                return;
+                //if  the capacity of stack is full
+                return false;//push failed
             }
             top1++;
             arr[top1]= num;
+            return true;//push is success
         }
-        public int pop() {
+        public boolean pop() {
             if(top1==-1){
-                return -1;
+                return false;
+                //Here setting -1 will have a conflict with popping a element -1
+                //so using return type as boolean is ideal .but it won't able to know what element got deleted.But can be found using top()
             }
             int val = arr[top1];
             top1--;
-            return val;
+            return true;
         }
         public int top() {
            if(top1 == -1){
-               return -1;
+                //Here setting -1 will have a conflict with top element as -1
+               return ;
            }
            return arr[top1];
         }
+        //both functions need to be using boolean return type
         public int isEmpty() {
             if(top1==-1)
                 return 1;
